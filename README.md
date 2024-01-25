b496f353e3d7cc1af78face6560d41a713e3195b
kvajjhula <kriti.vajjhula@gmail.com>
2024-01-25 12:14:19 -0800

    finish assignment

---
+++ README.md
@@ -0,0 +1,5 @@
+URL that returns the creative component metadata (keywords meta tag)
+
+https://www.youtube.com/watch?v=_RFBrasvNoA
+
+Visual improvements: I increased the thickness of the border, padding, added a margin. This makes the content more readable.
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,11 @@
 
 async function previewUrl(){
     let url = document.getElementById("urlInput").value;
+    console.log(url)
     
     let preview = "TODO: Fetch the url preview from your API"
     try {
-        const response = await fetch("api/v1/urls/preview/url?=" + url)
+        const response = await fetch("api/v1/urls/preview?url=" + url)
         if (response.ok) {
             preview = await response.text()
         }
